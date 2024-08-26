b0254a312b6a4fc17503f2efa609939dea9288cd
Mohit Panchariya <iammohitpanchariya@gmail.com>
2024-08-26 23:02:44 +0530

    Register routes separately

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,8 +8,9 @@ import (
 )
 
 type application struct {
-	infoLog  *log.Logger
-	errorLog *log.Logger
+	infoLog      *log.Logger
+	errorLog     *log.Logger
+	staticAssets string // Path to static assests
 }
 
 func main() {
@@ -21,23 +22,15 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	// Handler to serve static files
-	fileServer := http.FileServer(http.Dir(*static_dir))
-
 	app := &application{
-		infoLog:  infoLog,
-		errorLog: errorLog,
+		infoLog:      infoLog,
+		errorLog:     errorLog,
+		staticAssets: *static_dir,
 	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	srv := &http.Server{
 		Addr:     *addr,
-		Handler:  mux,
+		Handler:  app.routes(),
 		ErrorLog: errorLog,
 	}
 
