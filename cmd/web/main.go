8a3b7399c1e0070342218596fead1b421a406c74
Mohit Panchariya <iammohitpanchariya@gmail.com>
2024-09-01 20:01:13 +0530

    Setup database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -17,10 +20,23 @@ func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	static_dir := flag.String("static-dir", "./ui/static/", "Static assests path relative to project root")
+	dsn := flag.String("dsn", "", "MySQL Data Source Name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	if *dsn == "" {
+		errorLog.Fatal("dsn required")
+	}
+
+	// Databse connection pool
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		infoLog:      infoLog,
@@ -36,9 +52,23 @@ func main() {
 
 	infoLog.Printf("Starting server on %s\n", srv.Addr)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+}
+
+// Return a database connection pool. Returns an error if connection
+// pool can't be initialized or a ping check fails.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
