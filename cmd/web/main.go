70e0e9594c400e1c2b287014745e15e9781ee9e1
Mohit Panchariya <iammohitpanchariya@gmail.com>
2024-10-31 16:09:19 +0530

    Enable TLS and User Sessions
    The server now accepts connections only over HTTPS.
    User sessions are stored in the database and managed by a session
    manager(scs) on the server.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,26 +7,33 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/MohitPanchariya/Snippet-Box/internal/models"
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	infoLog       *log.Logger
-	errorLog      *log.Logger
-	staticAssets  string // Path to static assests
-	snippetModel  *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	infoLog        *log.Logger
+	errorLog       *log.Logger
+	staticAssets   string // Path to static assests
+	snippetModel   *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	static_dir := flag.String("static-dir", "./ui/static/", "Static assests path relative to project root")
 	dsn := flag.String("dsn", "", "MySQL Data Source Name")
+	certfile := flag.String("certfile", "", "TLS certifile path")
+	keyfile := flag.String("keyfile", "", "TLS keyfile path")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -34,6 +41,14 @@ func main() {
 
 	if *dsn == "" {
 		errorLog.Fatal("dsn required")
+	}
+
+	if *certfile == "" {
+		errorLog.Fatal("certfile required")
+	}
+
+	if *keyfile == "" {
+		errorLog.Fatal("keyfile required")
 	}
 
 	// Databse connection pool
@@ -49,13 +64,19 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	sessionManager := scs.New()
+	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
+
 	app := &application{
-		infoLog:       infoLog,
-		errorLog:      errorLog,
-		staticAssets:  *static_dir,
-		snippetModel:  &models.SnippetModel{DB: db},
-		templateCache: templateCache,
-		formDecoder:   form.NewDecoder(),
+		infoLog:        infoLog,
+		errorLog:       errorLog,
+		staticAssets:   *static_dir,
+		snippetModel:   &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    form.NewDecoder(),
+		sessionManager: sessionManager,
 	}
 
 	srv := &http.Server{
@@ -66,7 +87,7 @@ func main() {
 
 	infoLog.Printf("Starting server on %s\n", srv.Addr)
 
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS(*certfile, *keyfile)
 	if err != nil {
 		errorLog.Fatal(err)
 	}
