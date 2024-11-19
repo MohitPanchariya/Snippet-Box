8b1433998855112f777d22d1fb461b640f830e62
Mohit Panchariya <iammohitpanchariya@gmail.com>
2024-11-19 10:03:45 +0530

    Test SecureHeaders Middleware and ping handler
    Implemented a unit test for the secureHeaders middleware.
    Implemented an end to end test for the ping handler.

---
+++ cmd/web/handlers_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/MohitPanchariya/Snippet-Box/internal/assert"
+)
+
+func TestPing(t *testing.T) {
+	app := &application{
+		errorLog: log.New(io.Discard, "", 0),
+		infoLog:  log.New(io.Discard, "", 0),
+	}
+
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	rs, err := ts.Client().Get(ts.URL + "/ping")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rs.Body.Close()
+	body = bytes.TrimSpace(body)
+
+	assert.Equal(t, string(body), "OK")
+}
